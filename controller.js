729272d7b35db674d59f6f5321ebf0bc948a51df
alamasyarie <maiaelarsya@gmail.com>
2021-04-03 03:02:29 +0700

    menambahkan file controller,js

---
+++ controller.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const response = require("./res");
+const connection = require("./connection");
+
+exports.index = function (req, res) {
+  response.ok("Aplikasi restAPI berjalan");
+};
