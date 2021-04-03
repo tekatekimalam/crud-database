6f112c5c67bdff43af93d9464d1082cf54bc8db4
alamasyarie <maiaelarsya@gmail.com>
2021-04-03 19:27:05 +0700

    membuat controller dan routes berdasarkan id mahasiswa

--- controller.js
+++ controller.js
@@ -9,12 +9,19 @@ exports.index = function (req, res) {
 };
 
 // All data from mahasiswa
-exports.mahasiswa = function (req, res) {
+exports.mahasiswa = (req, res) => {
   connection.query("SELECT * FROM mahasiswa", (err, rows, fields) => {
-    if (err) {
-      connection.log(err);
-    } else {
-      response.ok(rows, res);
-    }
+    return err ? connection.log(err) : response.ok(rows, res);
   });
+};
+
+// Get data based on id_mahasiswa
+exports.id_mahasiswa = (req, res) => {
+  let id = req.params.id;
+  connection.query(
+    `SELECT * FROM mahasiswa WHERE id = ${id}`,
+    (err, rows, fields) => {
+      return err ? connection.log(err) : response.ok(rows, res);
+    },
+  );
 };
--- routes.js
+++ routes.js
@@ -6,4 +6,6 @@ module.exports = function (app) {
   app.route("/").get(json.index);
 
   app.route("/mahasiswa").get(json.mahasiswa);
+
+  app.route("/mahasiswa/:id").get(json.id_mahasiswa);
 };
